d5b2663cffa788fc5334fadeeddff9f470b724cc
gerryyang <diggwcdj@gmail.com>
2014-02-23 01:12:13 +0800

    add dir from local

---
+++ bin/Inc.sh
@@ -0,0 +1,8 @@
+# 2012-04-27 gerryyang
+# Inc.sh 
+# public bash file
+
+PROJECT_HOME=..
+
+#DATE=`date "+%Y-%m-%d %H:%M:%S"`
+DATE=`date "+%F %T"`
---
+++ bin/safe_test_svr.sh
@@ -0,0 +1,106 @@
+#!/bin/sh
+# modified by gerryyang 20120426
+
+# ================================================
+
+# include public script
+
+# [1] only for running scripts at current path
+#source Inc.sh
+
+# [2] make scripts all be relative to one another
+#MY_DIR=`dirname $0`
+#$MY_DIR/Inc.sh
+
+# [3] An alternative to [2]
+# The advantage being not having the dependence on dirname, 
+# which is not a built-in command
+#MY_DIR=${0%/*}
+#$MY_DIR/Inc.sh
+
+# [4] 
+source $(dirname $0)/Inc.sh
+
+# ================================================
+
+
+SERVER_PATH=$PROJECT_HOME/bin
+SERVER_CONF=$PROJECT_HOME/conf/test_svr.conf
+SERVER_PID=$PROJECT_HOME/bin/test_svr.pid
+SERVER=test_svr
+
+
+# save pid into PROC_ID
+# use CreatePIDFile function in code to create pid file 
+function GetPid()
+{
+  if [ -f $SERVER_PID ]; then
+    read PROC_ID < $SERVER_PID
+
+  else
+    PROC_ID=0
+  fi
+}
+
+function Boot()
+{
+    cd $SERVER_PATH; ./$SERVER $PROJECT_HOME
+    echo "$DATE $SERVER starts OK" 
+}
+
+function Start()
+{
+  if [ "$PROC_ID" == "0" ]; then
+  	# need to run server
+    Boot
+
+  else
+	# error, server has started
+  	echo "$DATE error:`basename "$PROC_ID"` already exist! use [$0 stop] first"
+  fi
+}
+
+function Stop()
+{
+  if [ "$PROC_ID" == "0" ]; then
+    echo "$DATE $SERVER is not running"
+
+  else
+	kill -s TERM $PROC_ID
+    echo "$DATE $SERVER has stopped"
+	# delete pid file
+    rm $SERVER_PID
+	# make sure that server has stopped
+	sleep 0.2; killall -9 $SERVER >/dev/null 2>&1
+  fi
+}
+
+function Reboot()
+{
+  Stop
+  sleep 2
+  Start
+}
+
+# run from here
+
+GetPid
+ulimit -c unlimited
+
+if [ "$1" == "start" ]; then
+  Start
+
+elif [ "$1" == "stop" ]; then
+  Stop
+
+elif [ "$1" == "reboot" ] || [ "$1" == "restart" ]; then
+  Reboot
+
+else
+  echo "$DATE usage: $0 (start|stop|reboot|restart)"
+  exit 1
+
+fi
+
+exit 0
+
