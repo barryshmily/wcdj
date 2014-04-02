e26add6ac3d44724578c53434b2288755a41d898
gerryyang <diggwcdj@gmail.com>
2014-04-02 23:12:44 +0800

    update date tools

--- tools/date/calc_date.sh
+++ tools/date/calc_date.sh
@@ -3,6 +3,12 @@
 ts=`date +%s`
 echo "current ts[$ts]"
 
+time_readalbe=`date -d@"$ts"`
+echo "time_readalbe[$time_readalbe]"
+
 echo "input seconds: "
 read sec
 date -d"1970-01-01 UTC $sec seconds" +"%Y-%m-%d %T %z"
+
+
+
