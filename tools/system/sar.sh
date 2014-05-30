c9acf455b7b03acf062524ba87bb6d3f6ff63b90
gerryyang <diggwcdj@gmail.com>
2014-05-30 22:27:49 +0800

    add sar tools

---
+++ tools/system/sar.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+:<< WCDJ
+sar有很多用途，如果要来监控网络流量，使用下面的命令行方式：
+
+sar -n DEV interval count
+
+其中，interval是统计时间间隔，以秒为单位；count是总共统计几次，如果为0就不断的统计直到 Ctrl+C 打断，否则执行count次就退出。
+
+比如：sar -n DEV 1 4
+比如：sar -n DEV 10 0
+
+IFACE：LAN接口
+rxpck/s：每秒钟接收的数据包
+txpck/s：每秒钟发送的数据包
+rxbyt/s：每秒钟接收的字节数
+txbyt/s：每秒钟发送的字节数
+WCDJ
+
+sar -n DEV 1 4
+
