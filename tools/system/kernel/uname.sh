a29f3a36bf3c521fec859dac17711cd5fbf70226
gerryyang <diggwcdj@gmail.com>
2014-06-02 10:17:01 +0800

    add uname tools

---
+++ tools/system/kernel/uname.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+:<< WCDJ
+uname - print system information
+Description
+Print certain system information. With no OPTION, same as -s.
+
+-s, --kernel-name      print the kernel name
+-n, --nodename         print the network node hostname
+-r, --kernel-release   print the kernel release
+-v, --kernel-version   print the kernel version
+-m, --machine          print the machine hardware name
+
+More: man uname
+
+WCDJ
+
+uname
+uname -s
+uname -n
+uname -r
+uname -v
+uname -m
+
