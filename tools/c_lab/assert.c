9b973ab440dffe5174a36b7d6ed0251a28aeb261
gerryyang <diggwcdj@gmail.com>
2014-06-15 21:56:19 +0800

    update

--- tools/c_lab/assert.c
+++ tools/c_lab/assert.c
@@ -1,16 +1,23 @@
 // test assert
 
+/* 在调试结束后，可以通过在#include <assert.h>语句之前插入#define NDEBUG来禁止assert调用
+ * */
+#define NDEBUG
 #include <assert.h>
 #include <stdio.h>
+
 
 int main(int argc, char **argv)
 {
 	FILE *fp = NULL;
 	fp = fopen("test.txt", "w");
+	printf("1[%p]\n", fp);
 	assert(fp);
 	fclose(fp);
 
+	fp = NULL;
 	fp = fopen("noexitfile.txt", "r");
+	printf("2[%p]\n", fp);
 	assert(fp);
 	fclose(fp);
 
@@ -19,6 +26,8 @@ int main(int argc, char **argv)
 /*
 output:
 ./assert 
-Assertion failed: (fp), function main, file assert.c, line 14.
+1[0x7fff7ba372a0]
+2[0x0]
+Assertion failed: (fp), function main, file assert.c, line 19.
 Abort trap: 6
  */
