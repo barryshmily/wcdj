a46eec29459959182d0ae5a727759179ac14d03b
gerryyang <diggwcdj@gmail.com>
2014-12-11 13:27:07 +0800

    update

---
+++ tools/bash/vote.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+while [ 1 ]; do
+curl -b"ASP.NET_SessionId=jsiqb4ngf3ltbwnvg014x245; info=6bRb7/8qqsgArSvS5PXUDe+PUl2/4dg9yrOziVkkiD8SD6Q9Wmtbu2gMnDPQcvZ53s5P9PYIVE9BTt6Gnn7OlA==; _ga=GA1.2.1235177319.1418223362; safedog-flow-item=7315FBE1CBF97A0184DE8C1BAAB72EEF" -d"q6851=o34956&ie-charset=%E0%B9%91&qNums=q6851%2C&property=737f8844-a173-4e35-aed8-d4c9b6e51b6d_1142_False_False&qText=&_ie-charset=%E0%B9%91" "http://vote3.cnhubei.com/survey-result"
+sleep 1;
+done
+
+:<< wcdj
+Accept	text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8
+Accept-Encoding	gzip, deflate
+Accept-Language	zh-cn,zh;q=0.8,en-us;q=0.5,en;q=0.3
+Connection	keep-alive
+Cookie	ASP.NET_SessionId=jsiqb4ngf3ltbwnvg014x245; info=6bRb7/8qqsgArSvS5PXUDe+PUl2/4dg9yrOziVkkiD8SD6Q9Wmtbu2gMnDPQcvZ53s5P9PYIVE9BTt6Gnn7OlA==; _ga=GA1.2.1235177319.1418223362; safedog-flow-item=7315FBE1CBF97A0184DE8C1BAAB72EEF
+Host	vote3.cnhubei.com
+Referer	http://www.cnhubei.com/xwzt/2014/d3jjc/
+User-Agent	Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:33.0) Gecko/20100101 Firefox/33.0
+wcdj
+
+
