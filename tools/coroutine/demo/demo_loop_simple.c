a504233f738e93b4660860d179155c20b04cc1e7
gerryyang <diggwcdj@gmail.com>
2014-12-28 22:11:16 +0800

    update

--- tools/coroutine/demo/demo_loop_simple.c
+++ tools/coroutine/demo/demo_loop_simple.c
@@ -5,14 +5,24 @@
 #include <stdio.h>
 #include <ucontext.h>
 #include <unistd.h>
+
+volatile int g_ivar = 1;
  
 int main(int argc, const char *argv[]){
 	ucontext_t context;
  
+	printf("before getcontext: g_ivar[%d]\n", g_ivar);
+
 	getcontext(&context);
+
+	g_ivar = 2;
+
+	printf("after getcontext: g_ivar[%d]\n", g_ivar);
+
 	puts("Hello world");
 	sleep(1);
 	setcontext(&context);
+
 	return 0;
 }
 /*
@@ -20,9 +30,13 @@ gcc -o demo_loop_simple demo_loop_simple.c -D_XOPEN_SOURCE -Wno-deprecated-decla
 
 output:
 gerryyang@mba:demo$./demo_loop_simple 
+before getcontext: g_ivar[1]
+after getcontext: g_ivar[2]
 Hello world
+after getcontext: g_ivar[2]
 Hello world
-Hello world
+after getcontext: g_ivar[2]
 Hello world
 ^C
+
  */
