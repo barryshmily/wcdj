562081b975159aadb92a233daa978d95814d8c78
gerryyang <diggwcdj@gmail.com>
2016-10-01 15:34:43 +0800

    update

--- tools/file/log/tmp_log.c
+++ tools/file/log/tmp_log.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#include <time.h>
 
 #define LOG(format, ...) \
 	do{ \
 		char __log_buff__[4096] = {0}; \
-		snprintf(__log_buff__, sizeof(__log_buff__), "[pid=%d][%s %d]" format"\n", getpid(), __FILE__, __LINE__, ##__VA_ARGS__); \
+		snprintf(__log_buff__, sizeof(__log_buff__), "[%d][%d][%s:%s:%d]" format"\n", (int)time(NULL), getpid(), __FILE__, __FUNCTION__, __LINE__, ##__VA_ARGS__); \
 		FILE* __fp=fopen("./tmp.log","a+"); \
 		fwrite(__log_buff__, strlen(__log_buff__), 1 ,__fp ); \
 		fclose(__fp); \
